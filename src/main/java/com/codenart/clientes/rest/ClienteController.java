9b1f908e76ce7d3f265a782d1fd59a7068e95220
Tiago Lima <tiagob@hotmail.com.br>
2021-02-20 10:19:35 -0300

    Endi-point encontrar cliente por id

--- src/main/java/com/codenart/clientes/rest/ClienteController.java
+++ src/main/java/com/codenart/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import com.codenart.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -22,4 +23,12 @@ public class ClienteController {
     public Cliente salvar (@RequestBody Cliente cliente) {
         return repository.save(cliente);
     }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId (@PathVariable Integer id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 }
